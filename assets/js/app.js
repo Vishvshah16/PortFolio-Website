b9c5fd1209a83f8e76b51e72d75bb9898fdd987d
Vishvshah16 <vishvshah0@gmail.com>
2024-04-17 21:33:11 +0530

    Initials

---
+++ assets/js/app.js
@@ -0,0 +1,145 @@
+
+window.addEventListener('load', fn, false)
+
+//  window.onload = function loader() {
+function fn() {
+    // Preloader
+    if (document.getElementById('preloader')) {
+        setTimeout(() => {
+            document.getElementById('preloader').style.visibility = 'hidden';
+            document.getElementById('preloader').style.opacity = '0';
+        }, 350);
+    }
+}
+
+function windowScroll() {
+    const navbar = document.getElementById("navbar");
+    if (
+        document.body.scrollTop >= 50 ||
+        document.documentElement.scrollTop >= 50
+    ) {
+        navbar.classList.add("is-sticky");
+    } else {
+        navbar.classList.remove("is-sticky");
+    }
+}
+
+window.addEventListener('scroll', (ev) => {
+    ev.preventDefault();
+    windowScroll();
+})
+
+// Navbar Active Class
+try {
+    var spy = new Gumshoe('#navbar-navlist a', {
+        offset: 80
+    });
+} catch (error) {
+
+}
+
+
+// Smooth scroll 
+try {
+    var scroll = new SmoothScroll('#navbar-navlist a', {
+        speed: 800,
+        offset: 80
+    });
+} catch (error) {
+
+}
+
+// Menu Collapse
+const toggleCollapse = (elementId, show = true) => {
+    const collapseEl = document.getElementById(elementId);
+    if (show) {
+        collapseEl.classList.remove('hidden');
+    } else {
+        collapseEl.classList.add('hidden');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Toggle target elements using [data-collapse]
+    document.querySelectorAll('[data-collapse]').forEach(function (collapseToggleEl) {
+        var collapseId = collapseToggleEl.getAttribute('data-collapse');
+
+        collapseToggleEl.addEventListener('click', function () {
+            toggleCollapse(collapseId, document.getElementById(collapseId).classList.contains('hidden'));
+        });
+    });
+});
+
+window.toggleCollapse = toggleCollapse;
+
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    var mybutton = document.getElementById("back-to-top");
+    if (mybutton != null) {
+        if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+            mybutton.classList.add("block");
+            mybutton.classList.remove("hidden");
+        } else {
+            mybutton.classList.add("hidden");
+            mybutton.classList.remove("block");
+        }
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+try {
+    document.addEventListener('DOMContentLoaded', function () {
+        const htmlTag = document.getElementsByTagName("html")[0];
+        htmlTag.className = 'dark';
+    });
+} catch (err) {
+    console.error(err);
+}
+
+
+try {
+    const htmlTag = document.getElementsByTagName("html")[0]
+    function changeLayout(e) {
+        e.preventDefault()
+        const switcherRtl = document.getElementById("switchRtl")
+        if (switcherRtl.innerText === "LTR") {
+            htmlTag.dir = "ltr"
+        }
+        else {
+            htmlTag.dir = "rtl"
+        }
+
+    }
+    const switcherRtl = document.getElementById("switchRtl")
+    switcherRtl?.addEventListener("click", changeLayout)
+}
+catch (err) { }
+
+feather.replace();
+
+
+(function () {
+    var current = location.pathname.substring(location.pathname.lastIndexOf('/') + 1);;
+    if (current === "") return;
+    var menuItems = document.querySelectorAll('.sidebar-nav a');
+    for (var i = 0, len = menuItems.length; i < len; i++) {
+        if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
+            menuItems[i].parentElement.className += " active";
+        }
+    }
+})();
+
+
+try {
+    new WOW().init();
+} catch (error) {
+
+}
